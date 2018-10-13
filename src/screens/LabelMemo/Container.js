630dece61289cf99e0a65f508dc0343753e6e7a0
pi3rrot <4dakto@gmail.com>
2018-10-13 13:45:42 +0900

    nested object state를 setstate하는 경우 immer를 사용하도록 수정.

--- src/screens/LabelMemo/Container.js
+++ src/screens/LabelMemo/Container.js
@@ -1,18 +1,67 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import produce from 'immer';
 import Presenter from './Presenter';
 
-class Container extends Component {
+class Container extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedMemoIds: [],
+      nameEditMode: {},
+      changedMemoTitle: {},
+    }
+    this.checkedMemoIds = new Set();
+  }
+
   componentDidMount() {
     this.init();
   }
 
+
   init = () => {
+
+  }
+
+  onChangeCheckedMemoIds = (memoId) => {
+    if (this.checkedMemoIds.has(memoId)) {
+      this.checkedMemoIds.delete(memoId);
+    } else {
+      this.checkedMemoIds.add(memoId);
+    }
+    this.setState({ checkedMemoIds: [...this.checkedMemoIds] })
+  }
+
+  onChangeTitle = (memoId, memoTitle = '') => {
+    this.setState(
+      produce(draft => {
+        draft.changedMemoTitle[memoId] = memoTitle;
+      })
+    );
+  }
+
+  onClickMemoTitleChange = (e, memoId) => {
+    e.preventDefault();
+
+    this.setState(
+      produce(draft => {
+        draft.nameEditMode[memoId] = !draft.nameEditMode[memoId];
+      }),
+      this.updateMemoTitle(memoId, this.state.changedMemoTitle[memoId])
+    );
+  }
+
+  updateMemoTitle = (memoId, changedMemoTitle) => {
+    this.props.requestUpdateMemo(memoId, changedMemoTitle);
   }
 
   render() {
     return (
       <Presenter
         {...this.props}
+        {...this.state}
+        onChangeCheckedMemoIds={this.onChangeCheckedMemoIds}
+        onClickMemoTitleChange={this.onClickMemoTitleChange}
+        onChangeTitle={this.onChangeTitle}
       />
     );
   }
