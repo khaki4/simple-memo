9be10b42865b6028b69b9720bbb4cf2c53b91f92
pi3rrot <4dakto@gmail.com>
2018-10-13 14:41:37 +0900

    라벨 메모 뷰 화면 정돈.

--- src/screens/LabelMemo/Presenter.js
+++ src/screens/LabelMemo/Presenter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment-timezone';
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ellipsisText } from '../../util/textUtil';
 import './styles.css';
 
@@ -8,12 +8,12 @@ import './styles.css';
 const Presenter = props => {
   const labelMemo = props.labelList[props.labelId] || {};
   return (
-    <div className="label-list">
+    <div className="label-memo">
       <h3>{labelMemo.title}</h3>
       <button onClick={props.onClickDeleteSeletedLabels}>선택 라벨 삭제</button>
       {labelMemo.memos && labelMemo.memos.map(memo => {
         return (
-          <div key={memo._id}>
+          <div key={memo._id} className="card">
             <form onSubmit={(e) => props.onClickMemoTitleChange(e, memo._id)}>
               <input
                 name="isSelectedMemo"
@@ -22,20 +22,22 @@ const Presenter = props => {
                 onChange={() => props.onChangeCheckedMemoIds(memo._id)}
               />
               <Link to={`/${props.labelId}/${memo._id}/`}>
-                <span>
-                  {
-                    props.nameEditMode[memo._id] ?
-                      (
-                        <input
-                          type="text"
-                          onChange={(e) => props.onChangeTitle(memo._id, e.target.value)}
-                          value={props.changedMemoTitle[memo._id] || ''}
-                        />
-                      ) :
-                      <span>{memo.title}</span>
-                  }
-                  <span>{moment(memo.updatedAt).format("YYYY. MM. DD.")}</span>
-                </span>
+                <div className="top">
+                  <span>
+                    {
+                      props.nameEditMode[memo._id] ?
+                        (
+                          <input
+                            type="text"
+                            onChange={(e) => props.onChangeTitle(memo._id, e.target.value)}
+                            value={props.changedMemoTitle[memo._id] || ''}
+                          />
+                        ) :
+                        <span>{ellipsisText(memo.title)}</span>
+                    }
+                    <span className="date">{moment(memo.updatedAt).format("YYYY. MM. DD.")}</span>
+                  </span>
+                </div>
                 <p>{ellipsisText(memo.content)}</p>
               </Link>
               <button onClick={(e) => props.onClickMemoTitleChange(e, memo._id)}>
